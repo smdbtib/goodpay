e98dfd52d17617232fce4d650bfb91634bbce0d7
smdbtib <smdb.tib@outlook.com>
2021-12-21 13:57:02 -0300

    Implemantação do módulo de comunicação e sua modulação.

--- src/index.js
+++ src/index.js
@@ -6,21 +6,14 @@ var myModal = new bootstrap.Modal(document.getElementById('myModal'));
 const btnLogin = document.getElementById("btnLogin");
 btnLogin.addEventListener("click", (evet) => {
     let userName = document.getElementById("user").value, 
-    password  = document.getElementById("password").value;
+        password  = document.getElementById("password").value;
+    let alertModal = controlerUserLogin.loginInfo(userName, password);
+    document.getElementById("titleModal").innerHTML = alertModal.title;
+    document.getElementById("bodyModal").innerHTML = alertModal.bodyModal;
+    document.getElementById("btnModalClose").innerHTML = alertModal.bt1;
+    document.getElementById("btnModalSave").innerHTML = alertModal.bt2;
 
-    if (!userName || userName === null || userName ==="" || !password || password===null || password==="") {
-        alert("Empty Fields!");
-        document.getElementById("titleModal").innerHTML = "Empty Fields!";
-        document.getElementById("bodyModal").innerHTML = "Fields cannot be empty!";
-        document.getElementById("btnModalClose").innerHTML = "Close";
-        myModal.show();
-    }else{
-        controlerUserLogin.loginInfo(userName, password);
-        document.getElementById("titleModal").innerHTML = "Success!";
-        document.getElementById("bodyModal").innerHTML = "User logged whit success!";
-        document.getElementById("btnModalClose").innerHTML = "Close";
-        myModal.show();
-    }
+    myModal.show();
 });
 
 const btnNewAccount = document.getElementById("btnNewAccount");
--- src/moduloInterface.js
+++ src/moduloInterface.js
@@ -1,4 +1,5 @@
 import userLogin from "./moduloDados.js";
+import communicationModal from "./moduloAlertDados.js";
 
 class ControlerUserLogin{
     constructor(status){
@@ -8,17 +9,20 @@ class ControlerUserLogin{
         this.status = true;
     }
     loginInfo(userName, password) {
-        var infoCheck = userLogin.find((userLogin) => userLogin.userName === userName);
-    
-        if(!infoCheck){
-            alert("User not found!")
-        }else{
-            console.log(infoCheck);
-            if (infoCheck.password === password) {
-                this.checkLogin();
-                alert(`Logged: ${this.status}` );
+        var checkUser = userLogin.find((userLogin) => userLogin.userName === userName);
+
+        if (userName === null || userName === "" || !password || password === null || password === "") {
+            return communicationModal.find((communicationModal) => communicationModal.idModal === "fieldsEmpyt1");
+        } else {
+            if (!checkUser) {
+                return communicationModal.find((communicationModal) => communicationModal.idModal === "userNotFound1");
             } else {
-                alert("Is not logged in!");
+                if (checkUser.password === password) {
+                    this.checkLogin();
+                    return communicationModal.find((communicationModal) => communicationModal.idModal === "loggedSuccess1");
+                } else {
+                    return communicationModal.find((communicationModal) => communicationModal.idModal === "loggedEror1");
+                }
             }
         }
     }
