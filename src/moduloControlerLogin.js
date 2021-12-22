c18a656f0162928aab3f16745e7e1dd740308ce3
smdbtib <smdb.tib@outlook.com>
2021-12-22 13:43:29 -0300

    Implementaçãio de controladores de rotas das páginas do aplicativo

--- src/moduloControlerLogin.js
+++ src/moduloControlerLogin.js
@@ -8,20 +8,32 @@ class ControlerUserLogin{
     checkLogin(){
         this.status = true;
     }
+    checkOutLogin(){
+        this.status = false;
+    }
     loginInfo(userName, password) {
+        localStorage.clear();
         var checkUser = userLogin.find((userLogin) => userLogin.userName === userName);
 
         if (userName === null || userName === "" || !password || password === null || password === "") {
+            this.checkOutLogin();
+            localStorage.status = this.status; /*LOCAL ESTORAGE FUNÇÃO NATIVA DO NAVEGADOR */
             return communicationModal.find((communicationModal) => communicationModal.idModal === "fieldsEmpyt1");
         } else {
             if (!checkUser) {
+                this.checkOutLogin();
+                localStorage.status = this.status;/*LOCAL ESTORAGE FUNÇÃO NATIVA DO NAVEGADOR */
                 return communicationModal.find((communicationModal) => communicationModal.idModal === "userNotFound1");
             } else {
                 if (checkUser.password === password) {
                     this.checkLogin();
+                    localStorage.status = this.status;/*LOCAL ESTORAGE FUNÇÃO NATIVA DO NAVEGADOR */
                     return communicationModal.find((communicationModal) => communicationModal.idModal === "loggedSuccess1");
                 } else {
+                    this.checkOutLogin();
+                    localStorage.status = this.status;/*LOCAL ESTORAGE FUNÇÃO NATIVA DO NAVEGADOR */
                     return communicationModal.find((communicationModal) => communicationModal.idModal === "loggedEror1");
+                    
                 }
             }
         }
